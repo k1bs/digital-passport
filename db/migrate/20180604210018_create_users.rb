2cc0241fcc60c9fb17ba75f5c661b8fbdc6f0ac5
Alex Kibler <alexkibler@me.com>
2018-06-04 15:02:23 -0600

    included token controller methods

---
+++ db/migrate/20180604210018_create_users.rb
@@ -0,0 +1,14 @@
+class CreateUsers < ActiveRecord::Migration[5.1]
+  def change
+    create_table :users do |t|
+      r.string :username, index: true
+      t.string :password_digest
+      t.string :email
+      t.string :first_name
+      t.string :last_name
+      t.string :auth_token
+
+      t.timestamps
+    end
+  end
+end
